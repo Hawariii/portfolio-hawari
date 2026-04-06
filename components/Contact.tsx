4cdadfb8d262fcca5ef2ca3aaf476e9bd898cc08
Ahmad hawari <ahawarii555@gmail.com>
2026-04-06 17:55:32 +0700

    ganti dan menambahkan kontak di section contact me

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,9 +4,10 @@ import { motion } from "framer-motion";
 
 const contacts = [
   { label: "GitHub", href: "https://github.com/Hawariii" },
-  { label: "Email", href: "mailto:hawari.dev@gmail.com" },
-  { label: "YouTube", href: "https://youtube.com/@siaizhu" },
-  { label: "Discord", href: "https://discord.com/users/hawari" },
+  { label: "Email", href: "mailto:ahawari.dev@gmail.com" },
+  { label: "Whatsapp", href: "https://wa.me/message/YPO3QEWF3JIVB1"}, 
+  { label: "Instagram", href: "https://www.instagram.com/hawari.undefined?igsh=cGwyenkxNTZsZHg=" },
+  { label: "Tiktok", href: "https://www.tiktok.com/@siiaizhu?_r=1&_d=ei1hdl45917jaf&sec_uid=MS4wLjABAAAAubmllnFcgqAB9cU0ATvefC0Nr_JZXptO7600U3gtMzmYc4KHIyhZXiPhJUcujd39&share_author_id=6942025009338598401&sharer_language=id&source=h5_t&u_code=dhhjaa751ka1lg&timestamp=1775472637&user_id=6942025009338598401&sec_user_id=MS4wLjABAAAAubmllnFcgqAB9cU0ATvefC0Nr_JZXptO7600U3gtMzmYc4KHIyhZXiPhJUcujd39&item_author_type=1&utm_source=copy&utm_campaign=client_share&utm_medium=android&share_iid=7622134152456587024&share_link_id=750e3ce4-2aeb-4560-a5f1-afab948cce25&share_app_id=1180&ugbiz_name=ACCOUNT&ug_btm=b8727%2Cb7360&social_share_type=5&enable_checksum=1" },
 ];
 
 export default function Contact() {
